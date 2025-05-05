c179f5ddba2f4e173d5c77ba11c33a5d2230b9bc
abdullah el baraka <145195566+AbdullahelBaraka@users.noreply.github.com>
2025-05-05 21:44:50 +0300

    Delete New folder - Copy/landing directory

--- New folder - Copy/landing/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-  // Set a timeout to hide the loading screen after both animations are done
-  setTimeout(() => {
-    document.getElementById('loading-screen').style.display = 'none';
-    document.getElementById('main-content').style.display = 'block';
-  }, 3700); // 0.8 seconds
-});
